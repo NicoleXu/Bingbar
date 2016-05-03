b453a009843e3f28f62c38298dcb13694308548b
xuxiaonan <xuxiaonan@lashou-inc.com>
2016-05-03 16:13:53 +0800

    nanbao update files

---
+++ js/bingbar.js
@@ -0,0 +1,13 @@
+function successively(){
+  var suobar = false;
+  var nanbar = true;
+  if(suobar){
+    //balabalbala
+    suobar = false;
+    nanbar = true;
+  }else{
+    //balabalbala
+    nanbar = false;
+    suobar =true;
+  }
+}
