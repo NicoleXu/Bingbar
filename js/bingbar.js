44adf58b548c0f5fb59a38cd9f262464d1e30f19
xuxiaonan <xuxiaonan@lashou-inc.com>
2016-05-03 18:17:55 +0800

    update input chose feature

--- js/bingbar.js
+++ js/bingbar.js
@@ -1,13 +1,32 @@
-function successively(){
-  var suobar = false;
-  var nanbar = true;
-  if(suobar){
-    //balabalbala
-    suobar = false;
-    nanbar = true;
-  }else{
-    //balabalbala
-    nanbar = false;
-    suobar =true;
-  }
+// function successively() {
+//     var suobar = false;
+//     var nanbar = true;
+//     if (suobar) {
+//         //balabalbala
+//         suobar = false;
+//         nanbar = true;
+//     } else {
+//         //balabalbala
+//         nanbar = false;
+//         suobar = true;
+//     }
+// }
+
+window.onload = function() {
+    function chose() {
+        var person_1 = document.getElementById('person_1');
+        var person_2 = document.getElementById('person_2');
+        // body...
+        if (person_1.checked == true) {
+            suobar = true;
+            nanbar = false;
+            alert("aha suo bao frist");
+        } else {
+            nanbar = true;
+            suobar = false;
+            alert("aha nan bao frist");
+        }
+    }
+    // body...
+    chose();
 }
